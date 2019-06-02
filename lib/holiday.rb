d1d3b555b3830b7192b826c562dcba990bd7ec05
Ahmed Gaber <21stlimitless@gmail.com>
2019-06-02 04:45:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,10 +74,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  array =[]
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supplies|
-      
-
+      array << holiday
+    end
+  end
+  array
 end
 
 
